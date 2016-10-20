f1d245c9ebc4501e7797b5dabedd2d35e9d7d947
JohnnyTam <shrimpcolo@gmail.com>
2016-10-20 16:30:19 +0800

    路由功能，完成一小部分，存在html文件找不到问题

--- ng2-tour-of-heroes/app/app.module.ts
+++ ng2-tour-of-heroes/app/app.module.ts
@@ -1,14 +1,41 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 import {AppComponent} from './app.component';
-import {HeroDetailComponent} from './hero-detail.component'
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroesComponent} from './heroes.component';
+import {HeroService} from "./hero.service";
+import {DashboardComponent} from './dashboard.component';
 
 @NgModule({
-	imports: [BrowserModule, FormsModule],
-	declarations: [AppComponent, HeroDetailComponent],
-	bootstrap: [AppComponent]
+	imports: [
+		BrowserModule,
+		FormsModule,
+		RouterModule.forRoot([
+			{
+				path: 'heroes',
+				component: HeroesComponent
+			},
+			{
+				path: 'dashboard',
+				component: DashboardComponent
+			},
+			{
+				path: '',
+				redirectTo: '/dashboard',
+				pathMatch: 'full'
+			},
+		])
+	],
+	declarations: [AppComponent,
+		HeroDetailComponent,
+		HeroesComponent,
+		DashboardComponent
+	],
+	bootstrap: [AppComponent],
+	providers: [HeroService]
 })
 
 export class AppModule {}
---
+++ ng2-tour-of-heroes/app/dashboard.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-dashboard',
+    //template: '<h3>My Dashboard</h3>'
+    templateUrl: 'dashboard.component.html'
+})
+export class DashboardComponent{}
