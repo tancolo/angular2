7e5bf1cb50b94b703aa9ffb08922c81f9b7fd64e
JohnnyTam <shrimpcolo@gmail.com>
2016-10-25 16:40:15 +0800

    step 6, 重构路由为一个 路由模块

--- ng2-tour-of-heroes/app/app.module.ts
+++ ng2-tour-of-heroes/app/app.module.ts
@@ -1,42 +1,26 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
 
 import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard.component';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroesComponent} from './heroes.component';
 import {HeroService} from "./hero.service";
-import {DashboardComponent} from './dashboard.component';
+
+import {AppRoutingModule} from './app.routing.module';
 
 @NgModule({
 	imports: [
 		BrowserModule,
 		FormsModule,
-		RouterModule.forRoot([
-			{
-				path: 'heroes',
-				component: HeroesComponent
-			},
-			{
-				path: 'dashboard',
-				component: DashboardComponent
-			},
-			{
-				path: '',
-				redirectTo: '/dashboard',
-				pathMatch: 'full'
-			},
-			{
-				path: 'detail/:id',
-				component: HeroDetailComponent
-			}
-		])
+		AppRoutingModule
 	],
-	declarations: [AppComponent,
+	declarations: [
+		AppComponent,
+		DashboardComponent,
 		HeroDetailComponent,
-		HeroesComponent,
-		DashboardComponent
+		HeroesComponent
 	],
 	bootstrap: [AppComponent],
 	providers: [HeroService]
