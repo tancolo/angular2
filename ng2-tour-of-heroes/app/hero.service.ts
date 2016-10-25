32a08c0a4bc0c18e2702ea190b92f9db307d9767
JohnnyTam <shrimpcolo@gmail.com>
2016-10-25 23:13:05 +0800

    step 7, 删除一个英雄
           modified:   ng2-tour-of-heroes/app/hero.service.ts
           modified:   ng2-tour-of-heroes/app/heroes.component.ts
           modified:   ng2-tour-of-heroes/app/view/heroes.component.css
           modified:   ng2-tour-of-heroes/app/view/heroes.component.html

--- ng2-tour-of-heroes/app/hero.service.ts
+++ ng2-tour-of-heroes/app/hero.service.ts
@@ -55,4 +55,11 @@ export class HeroService{
         .catch(this.handleError);
     }
 
+    delete(id: number): Promise<void> {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.delete(url, {headers: this.headers})
+        .toPromise()
+        .then(() => null)
+        .catch(this.handleError);
+    }
 }
--- ng2-tour-of-heroes/app/heroes.component.ts
+++ ng2-tour-of-heroes/app/heroes.component.ts
@@ -50,4 +50,15 @@ export class HeroesComponent implements OnInit {
             this.selectedHero = null;
         });
     }
+
+    delete(hero: Hero): void {
+        this.heroService
+        .delete(hero.id)
+        .then(() => {
+            this.heroes = this.heroes.filter(h => h != hero);
+            if(this.selectedHero === hero){
+                this.selectedHero = null;
+            }
+        })
+    }
 }
