4905f0fa95885ea42177e180a14a44c40608b8d8
JohnnyTam <shrimpcolo@gmail.com>
2016-11-01 23:36:53 +0800

    完成webpack-dev-server安装 以及脚本的添加。

--- ng2-starter-webpack/src/app/app.component.ts
+++ ng2-starter-webpack/src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My App</h1>'
+    template: '<h1>My App by 222</h1>'
 })
 
 export class AppComponent{}
