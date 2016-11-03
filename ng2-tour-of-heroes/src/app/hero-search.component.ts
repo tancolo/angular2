00d729670aa0d8da4f19227bc7888c2bc4288aa4
JohnnyTam <shrimpcolo@gmail.com>
2016-11-03 11:15:20 +0800

    使用webpack打包 tour-of-hero 英雄指南

---
+++ ng2-tour-of-heroes/src/app/hero-search.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {Observable} from 'rxjs/observable';
+import {Subject} from 'rxjs/subject';
+
+import {HeroSearchService} from './hero-search.service';
+import {Hero} from './hero';
+
+@Component({
+    selector: 'hero-search',
+    templateUrl: './hero-search.component.html',
+    styleUrls: ['./hero-search.component.css'],
+    providers: [HeroSearchService]
+})
+export class HeroSearchComponent implements OnInit {
+    heroes: Observable<Hero[]>;
+    private searchTerms = new Subject<string>();
+
+    constructor(
+        private heroSearchService: HeroSearchService,
+        private router: Router){}
+
+    search(term: string): void{
+        this.searchTerms.next(term);
+    }
+
+    ngOnInit():void {
+        this.heroes = this.searchTerms
+        .debounceTime(300)
+        .distinctUntilChanged()
+        .switchMap(term =>
+            term ? this.heroSearchService.search(term) : Observable.of<Hero[]>([]))
+        .catch(error => {
+            console.log(error);
+            return Observable.of<Hero[]>([])
+        });
+    }
+
+    gotoDetail(hero: Hero): void{
+        let link = ['/detail', hero.id];
+        this.router.navigate(link);
+    }
+}
---
+++ ng2-tour-of-heroes/webpack.config.js
@@ -0,0 +1,57 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+
+    entry: './src/app/main.ts',
+    output: {
+        path: root('dist'),
+        filename: 'app.bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.ts']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+            },
+            {
+                test: /\.css$/,
+                exclude: root('src', 'app'),
+                loader: ExtractTextPlugin.extract({fallbackLoader: 'style-loader', loader: ['css']})
+            },
+
+            // all css required in src/app files will be merged in js files
+            {
+                test: /\.css$/,
+                include: root('src', 'app'),
+                loader: 'raw'
+            },
+
+            // support for .html as raw text
+            {
+                test: /\.html$/,
+                loader: 'raw',
+                exclude: root('src', 'index.html'),
+                include: root('src', 'app')
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new ExtractTextPlugin({filename: 'css/[name].[hash].css'})
+    ]
+};
+
+// Helper functions
+function root(args) {
+    args = Array.prototype.slice.call(arguments, 0);
+    return path.join.apply(path, [__dirname].concat(args));
+}
