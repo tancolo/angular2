587772c9a825b5936979d1e285160ed8ea006785
JohnnyTam <shrimpcolo@gmail.com>
2016-12-05 18:30:01 +0900

    基本的Jquery引入

--- ng2-starter-webpack/src/app/app.component.ts
+++ ng2-starter-webpack/src/app/app.component.ts
@@ -1,8 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild, ElementRef, OnInit, AfterViewInit} from '@angular/core';
+declare var $: any;
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My App by 222</h1>'
+    template: `<select #selectElem>
+        <option *ngFor="let item of items" [value]="item" [selected]="item === selectedValue">{{item}} option</option>
+        </select>
+        <h4> {{selectedValue}}</h4>`
 })
 
-export class AppComponent{}
+export class AppComponent implements AfterViewInit{
+    @ViewChild('selectElem') el:ElementRef;
+    items = ['First', 'Second', 'Third'];
+    selectedValue = 'Second';
+
+    ngAfterViewInit(): void {
+        $(this.el.nativeElement)
+            .chosen()
+            .on('change', (e, args) => {
+                this.selectedValue = args.selected;
+                console.log('selectedValue: ', this.selectedValue);
+            });
+    }
+}
