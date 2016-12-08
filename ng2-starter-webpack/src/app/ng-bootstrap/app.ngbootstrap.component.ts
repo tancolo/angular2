d0950f55d2e3f402cdaca0b97a089cdb0d9ff580
JohnnyTam <shrimpcolo@gmail.com>
2016-12-08 14:08:48 +0800

    ng-bootstrap datepicker使用

--- ng2-starter-webpack/src/app/ng-bootstrap/app.ngbootstrap.component.ts
+++ ng2-starter-webpack/src/app/ng-bootstrap/app.ngbootstrap.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {I18n} from "./custom.datepickerI18n";
-
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 @Component({
     selector: 'ngbs-app',
     templateUrl: './app.ngbootstrap.component.html'
@@ -8,7 +8,7 @@ import {I18n} from "./custom.datepickerI18n";
 
 export class AppNgBootstrapComponent {
     model;
-    date: {year: number, month: number, day: number};
+    date: NgbDateStruct;
 
     constructor(private _i18n: I18n) {
         this.date = {year: 2015, month: 12, day: 8};
