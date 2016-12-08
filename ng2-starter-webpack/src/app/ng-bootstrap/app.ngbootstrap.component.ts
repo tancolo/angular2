51082fbca898efef5d8bbdce58078b46e2eaf0ac
JohnnyTam <shrimpcolo@gmail.com>
2016-12-08 13:14:59 +0900

    datepicker 显示中文月份 配置

--- ng2-starter-webpack/src/app/ng-bootstrap/app.ngbootstrap.component.ts
+++ ng2-starter-webpack/src/app/ng-bootstrap/app.ngbootstrap.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {I18n} from "./custom.datepickerI18n";
 
 @Component({
     selector: 'ngbs-app',
@@ -7,4 +8,18 @@ import {Component} from '@angular/core';
 
 export class AppNgBootstrapComponent {
     model;
+    date: {year: number, month: number, day: number};
+
+    constructor(private _i18n: I18n) {
+        this.date = {year: 2015, month: 12, day: 8};
+    }
+
+    set language(lang: string){
+        this._i18n.language = lang;
+    }
+
+    get language(){
+        return this._i18n.language;
+    }
+
 }
